Fixed sign on influence calculations

CIFAR10/influence.py
@@ -143,8 +143,8 @@ for i in range(len(image)):
     i_pert = grad(infl,x)
     i_pert = i_pert[0].view(1,3,32,32)
 
-    eqn_2 = np.vstack((eqn_2,infl.detach().cpu().numpy())) if eqn_2.size else infl.detach().cpu().numpy()
-    eqn_5 = np.vstack((eqn_5,i_pert.detach().cpu().numpy())) if eqn_5.size else i_pert.detach().cpu().numpy()
+    eqn_2 = np.vstack((eqn_2,-infl.detach().cpu().numpy())) if eqn_2.size else -infl.detach().cpu().numpy()
+    eqn_5 = np.vstack((eqn_5,-i_pert.detach().cpu().numpy())) if eqn_5.size else -i_pert.detach().cpu().numpy()
     
     
 sort = np.argsort(eqn_2.reshape(-1))

MNIST/influence.py
@@ -12,42 +12,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torch.autograd import grad
 
-class Model(torch.nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.conv_1 = torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.linear_1 = torch.nn.Linear(25088, 100)
-        # self.linear_2 = torch.nn.Linear(100,10)
-        self.selu = torch.nn.SELU()
-        self.softmax = torch.nn.Softmax(dim=1)
-
-    def forward(self, x):
-        x = self.conv_1(x)
-        x = self.selu(x)
-        x = x.reshape(x.size(0), -1)
-        x = self.linear_1(x)
-        # x = self.selu(x)
-        # x = self.linear_2(x)
-        pred = self.softmax(x)
-
-        return pred
-
 # class Model(torch.nn.Module):
 #     def __init__(self):
 #         super(Model, self).__init__()
-#         self.linear_1 = torch.nn.Linear(784, 100)
-#         self.linear_2 = torch.nn.Linear(100,10)
+#         self.conv_1 = torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
+#         self.linear_1 = torch.nn.Linear(25088, 100)
+#         # self.linear_2 = torch.nn.Linear(100,10)
 #         self.selu = torch.nn.SELU()
 #         self.softmax = torch.nn.Softmax(dim=1)
 
 #     def forward(self, x):
+#         x = self.conv_1(x)
+#         x = self.selu(x)
 #         x = x.reshape(x.size(0), -1)
 #         x = self.linear_1(x)
-#         x = self.selu(x)
-#         x = self.linear_2(x)
+#         # x = self.selu(x)
+#         # x = self.linear_2(x)
 #         pred = self.softmax(x)
 
 #         return pred
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear_1 = torch.nn.Linear(784, 100)
+        self.linear_2 = torch.nn.Linear(100,10)
+        self.selu = torch.nn.SELU()
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = x.reshape(x.size(0), -1)
+        x = self.linear_1(x)
+        x = self.selu(x)
+        x = self.linear_2(x)
+        pred = self.softmax(x)
+
+        return pred
 
 # class Model(torch.nn.Module):
 #     def __init__(self):
@@ -74,7 +74,7 @@ def hessian_vector_product(ys,xs,v):
     J.backward(v,retain_graph=True)
     return xs.grad
 
-model_type = 'cnn'
+model_type = 'mlp'
 model = torch.load(model_type +'.pt')
 
 mnist_trainset = torch.load('mnist_trainset.pt')
@@ -166,8 +166,8 @@ for i in range(len(image)):
     i_pert = grad(infl,x)
     i_pert = i_pert[0].view(1,28,28)
 
-    eqn_2 = np.vstack((eqn_2,infl.detach().cpu().numpy())) if eqn_2.size else infl.detach().cpu().numpy()
-    eqn_5 = np.vstack((eqn_5,i_pert.detach().cpu().numpy())) if eqn_5.size else i_pert.detach().cpu().numpy()
+    eqn_2 = np.vstack((eqn_2,-infl.detach().cpu().numpy())) if eqn_2.size else -infl.detach().cpu().numpy()
+    eqn_5 = np.vstack((eqn_5,-i_pert.detach().cpu().numpy())) if eqn_5.size else -i_pert.detach().cpu().numpy()
     
     
 sort = np.argsort(eqn_2.reshape(-1))

More Intuition/influence_extended_simple.py
@@ -75,7 +75,7 @@ for i in range(recursion_depth):
     hvp = hessian_vector_product(train_loss, model.lin2.weight, cur_estimate)
     cur_estimate = [a+(1-damping)*b-c/scale for (a,b,c) in zip(v,cur_estimate,hvp)]
     cur_estimate = torch.squeeze(torch.stack(cur_estimate)).view(1,-1)
-
+    model.zero_grad()
     if (i % print_iter ==0) or (i==recursion_depth-1):
         numpy_est = cur_estimate[0].detach().numpy()
         numpy_est = numpy_est.reshape(1,-1)####
@@ -93,7 +93,7 @@ eqn_5 = np.array([])
 ihvp = ihvp.detach()
 for i in range(len(train_X)):
     x = torch.tensor(train_X[i],requires_grad=True)
-    # model.zero_grad()
+    model.zero_grad()
     x0,x1,x2,x_out = model.forward_with_stages(x)
     x_loss = criterion(x_out,train_y[0])
     x_loss.backward(create_graph=True)
@@ -103,8 +103,8 @@ for i in range(len(train_X)):
     infl = (torch.dot(ihvp.view(1,-1).squeeze(),grads.view(-1,1).squeeze())/num_train_samples)
     i_pert = grad(infl,x)
 
-    eqn_2 = np.vstack((eqn_2,infl.detach().numpy())) if eqn_2.size else infl.detach().numpy()
-    eqn_5 = np.vstack((eqn_5,i_pert[0].detach().numpy())) if eqn_5.size else i_pert[0].detach().numpy()
+    eqn_2 = np.vstack((eqn_2,-infl.detach().numpy())) if eqn_2.size else -infl.detach().numpy()
+    eqn_5 = np.vstack((eqn_5,-i_pert[0].detach().numpy())) if eqn_5.size else -i_pert[0].detach().numpy()
 
-# np.save('results/eqn_2_extended_lin2.npy',eqn_2)
-# np.save('results/eqn_5_extended_lin2.npy',eqn_5)
+np.save('results/eqn_2_extended_lin2.npy',eqn_2)
+np.save('results/eqn_5_extended_lin2.npy',eqn_5)