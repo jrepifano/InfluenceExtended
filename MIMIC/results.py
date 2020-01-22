2e47b708e5d227614535637d37f3c7f42a0b3ca9
jrepifano <jrepifano@gmail.com>
2020-01-22 01:47:07 -0500

    top 5 -> top 15 features

--- MIMIC/results.py
+++ MIMIC/results.py
@@ -17,11 +17,21 @@ eqn_5_sum = np.sum(eqn_5,axis=0)
 eqn_5_sort = np.argsort(eqn_5_sum)
 coefs_sort = np.argsort(abs(coefs))[0]
 
-top_5_infl = np.flip(eqn_5_sort[-5:])
-top_5_coefs = np.flip(coefs_sort[-5:])
+top_5_infl = np.flip(eqn_5_sort[-15:])
+top_5_coefs = np.flip(coefs_sort[-15:])
 
 print(feat_names[top_5_infl])
 print(feat_names[top_5_coefs])
 
-#Influence top 5['INR_max' 'BICARBONATE_min' 'SODIUM_max' 'WBC_max' 'POTASSIUM_min']
-#Log Reg top 5['BICARBONATE_min' 'CHLORIDE_min' 'LACTATE_max' 'WBC_min' 'WBC_max']
+
+
+Influence Functions top 15 features:
+
+['INR_max' 'BICARBONATE_min' 'SODIUM_max' 'WBC_max' 'POTASSIUM_min'
+ 'PLATELET_min' 'alt' 'day1pao2' 'CHLORIDE_min' 'CREATININE_max' 'age'
+ 'calcium' 'BUN_max' 'ALBUMIN_min' 'POTASSIUM_max']
+
+Logistic Regression top 15 features:
+['BICARBONATE_min' 'CHLORIDE_min' 'LACTATE_max' 'WBC_min' 'WBC_max'
+ 'HEMATOCRIT_min' 'BUN_min' 'BUN_max' 'SODIUM_max' 'CHLORIDE_max'
+ 'BILIRUBIN_max' 'PT_max' 'BANDS_max' 'SODIUM_min' 'age']
