re-did everything with scaling data leak fixed.

eICU/mlp_influence.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from torch.autograd import grad
 import time
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
 
 class Model(torch.nn.Module):
     def __init__(self):
@@ -38,7 +40,7 @@ def hessian_vector_product(ys,xs,v):
     
 r = np.random.RandomState(seed=1234567890)
 
-x_scaled = np.load('data/x_scaled.npy')
+x_imputed = np.load('data/x_imputed.npy')
 y = np.load('data/y.npy')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -47,7 +49,7 @@ if(torch.cuda.is_available()):
 
 model = Model()
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.5, momentum=0.9)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.05, momentum=0.9)
 
 mlp_probs = np.array([])
 test_labels = np.array([])
@@ -58,7 +60,15 @@ train_loss = list()
 val_loss = list()
 best_val_loss = 1
 
-x_train,x_test,y_train,y_test = train_test_split(x_scaled,y,test_size=0.3333,random_state=r)
+sm = SMOTE()
+
+x_train,x_test,y_train,y_test = train_test_split(x_imputed,y,test_size=0.1,random_state=r)
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+    
+# x_train, y_train = sm.fit_resample(x_train, y_train)
 
 x_train = torch.from_numpy(x_train).float().to(device)
 x_test = torch.from_numpy(x_test).float().to(device)
@@ -162,8 +172,8 @@ for i in range(len(x_train)):
     eqn_5 = np.vstack((eqn_5,-i_pert.detach().cpu().numpy())) if eqn_5.size else -i_pert.detach().cpu().numpy()
     model.zero_grad()
     
-np.save('results/eqn_2-test_set.npy',eqn_2)
-np.save('results/eqn_5-test_set.npy',eqn_5)
+np.save('results/eqn_2-test_set-no_smote.npy',eqn_2)
+np.save('results/eqn_5-test_set-no_smote.npy',eqn_5)
 
 elapsed_time = time.time()-start_time
-np.save('results/mlp_influence_time',elapsed_time)
+# np.save('results/mlp_influence_time',elapsed_time)

eICU/mlp_shap.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 import matplotlib.pyplot as plt
 import time
+from sklearn.preprocessing import StandardScaler
 
 class Model(torch.nn.Module):
     def __init__(self):
@@ -35,11 +36,12 @@ class Model(torch.nn.Module):
     
 r = np.random.RandomState(seed=1234567890)
 
-x_scaled = np.load('data/x_scaled.npy')
+x_imputed = np.load('data/x_imputed.npy')
 y = np.load('data/y.npy')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if(torch.cuda.is_available()):
+    torch.cuda.set_device(1)
     print('Using device:', torch.cuda.get_device_name(torch.cuda.current_device()))
 
 model = Model()
@@ -57,9 +59,13 @@ best_val_loss = 1
 
 sm = SMOTE()
 
-x_train,x_test,y_train,y_test = train_test_split(x_scaled,y,test_size=0.3333,random_state=r)
+x_train,x_test,y_train,y_test = train_test_split(x_imputed,y,test_size=0.3333,random_state=r)
 
-# x_train, y_train = sm.fit_resample(x_train, y_train)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+    
+x_train, y_train = sm.fit_resample(x_train, y_train)
 
 x_train = torch.from_numpy(x_train)
 x_test = torch.from_numpy(x_test)
@@ -124,7 +130,7 @@ e = shap.DeepExplainer(model, x_train.float().to(device))
 shap_values = e.shap_values(x_test.float().to(device))
 shap.summary_plot(shap_values, x_train.float().to(device), plot_type="bar")
 
-np.save('results/mlp_shap_values.npy',shap_values)
+np.save('results/mlp_shap_values_smote.npy',shap_values)
 
 elapsed_time = time.time()-start_time
-np.save('results/mlp_shap_time',elapsed_time)
+# np.save('results/mlp_shap_time',elapsed_time)

eICU/model_selection.py
@@ -24,9 +24,9 @@ r = np.random.RandomState(seed=1234567890)
 x = np.load('data/x.npy')
 y = np.load('data/y.npy')
 x_imputed = np.load('data/x_imputed.npy')
-x_scaled = np.load('data/x_scaled.npy')
+# x_scaled = np.load('data/x_scaled.npy')
 
-feat_names = np.load('data/column_names.npy',allow_pickle=True)
+# feat_names = np.load('data/column_names.npy',allow_pickle=True)
 
 ## Corr matrix plot
 # corr_matrix = pd.DataFrame(x).corr(method = "spearman").abs()
@@ -38,18 +38,16 @@ feat_names = np.load('data/column_names.npy',allow_pickle=True)
 # plt.savefig("results/correlation_matrix.png", dpi = 1080)
 
 ## Impute with MICE
-imputer = IterativeImputer()
-x_imputed = imputer.fit_transform(x)
-np.save('data/x_imputed.npy',x_imputed)
+# imputer = IterativeImputer()
+# x_imputed = imputer.fit_transform(x)
+# np.save('data/x_imputed.npy',x_imputed)
 
 ## Standardize Data
-scaler = StandardScaler()
-x_scaled = scaler.fit_transform(x_imputed)
-np.save('data/x_scaled.npy',x_scaled)
+# np.save('data/x_scaled.npy',x_scaled)
 
 ## Mutual Information Scores
-mi_score = mutual_info_classif(x_scaled,y)
-np.save('results/mi_scores.npy',mi_score)
+# mi_score = mutual_info_classif(x_scaled,y)
+# np.save('results/mi_scores.npy',mi_score)
 
 ##Training Loop Starts
 logReg_params = {'penalty':['l1','l2'],'C':[0.01,0.1,1,10,100],'solver':['liblinear'],'random_state':[r]}
@@ -64,33 +62,38 @@ coefs = np.array([])
 test_labels = np.array([])
 i = 1
 print('Starting CV Loop')
-for train_index,test_index in kf.split(x_scaled):
-    # x_resampled,y_resampled = SMOTE().fit_resample(x_scaled[train_index],y[train_index])
-    x_resampled = x_scaled[train_index]
-    y_resampled = y[train_index]
+for train_index,test_index in kf.split(x_imputed):
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_imputed[train_index])
+    y_train = y[train_index]
+    
+    x_test = scaler.transform(x_imputed[test_index])
+    y_test = y[test_index]
+    
+    x_train,y_train = SMOTE().fit_resample(x_train,y_train)
     
     logReg = LogisticRegression()
     xg = xgboost.XGBClassifier()
     # print('Starting LogReg grid search')
-    # clf1 = GridSearchCV(logReg,logReg_params,make_scorer(roc_auc_score),iid=False,cv=5,refit=True,n_jobs=-1).fit(x_resampled,y_resampled)
+    # clf1 = GridSearchCV(logReg,logReg_params,make_scorer(roc_auc_score),iid=False,cv=5,refit=True,n_jobs=-1).fit(x_train,y_train)
     print('Starting xgb grid search')
-    clf2 = GridSearchCV(xg,xgb_params,make_scorer(roc_auc_score),iid=False,cv=5,refit=True,n_jobs=3).fit(x_resampled,y_resampled)
+    clf2 = GridSearchCV(xg,xgb_params,make_scorer(roc_auc_score),iid=False,cv=5,refit=True,n_jobs=3).fit(x_train,y_train)
 
-    # pd.DataFrame(clf1.cv_results_).to_csv('results/logreg_cv_results_'+str(i)+'.csv')
-    pd.DataFrame(clf2.cv_results_).to_csv('results/xgb_cv_results_'+str(i)+'.csv')
+    # pd.DataFrame(clf1.cv_results_).to_csv('results/tuning_params/smote/logreg_cv_results_'+str(i)+'.csv')
+    pd.DataFrame(clf2.cv_results_).to_csv('results/xgb_smote_cv_results_'+str(i)+'.csv')
     
-    # probs_1 = clf1.predict_proba(x_scaled[test_index])
-    probs_2 = clf2.predict_proba(x_scaled[test_index])
+    # probs_1 = clf1.predict_proba(x_test)
+    probs_2 = clf2.predict_proba(x_test)
     
     # logreg_probs = np.vstack((logreg_probs,probs_1)) if logreg_probs.size else probs_1
     xgb_probs = np.vstack((xgb_probs,probs_2)) if xgb_probs.size else probs_2
     # coefs = np.vstack((coefs,clf1.best_estimator_.coef_)) if coefs.size else clf1.best_estimator_.coef_
-    test_labels = np.hstack((test_labels,y[test_index])) if test_labels.size else y[test_index]
+    test_labels = np.hstack((test_labels,y_test)) if test_labels.size else y_test
     
     print('Done Fold: '+str(i) +'/5')
     i+=1
     
-# np.save('results/logreg_probs_no_resampling.npy',logreg_probs)
-np.save('results/xgb_probs_no_resampling.npy',xgb_probs)
-# np.save('results/coefs_no_resampling.npy',coefs)
-np.save('results/probs/test_labels_xgb_no_resampling.npy',test_labels)
+# np.save('results/probs/smote/logreg_probs.npy',logreg_probs)
+np.save('results/probs/smote/xgb_prob.npy',xgb_probs)
+# np.save('results/coefs_smote.npy',coefs)
+np.save('results/probs/smote/test_labels_xgb.npy',test_labels)

eICU/train_mlp.py
@@ -11,6 +11,7 @@ import torch
 from sklearn.model_selection import KFold
 from imblearn.over_sampling import SMOTE
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 class Model(torch.nn.Module):
     def __init__(self):
@@ -31,7 +32,7 @@ class Model(torch.nn.Module):
 
         return pred
 
-x_scaled = np.load('data/x_scaled.npy')
+x_imputed = np.load('data/x_imputed.npy')
 y = np.load('data/y.npy')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -46,17 +47,23 @@ no_epochs = 2000
 
 kf = KFold(n_splits=5)
 
-print_iter = 500
+print_iter = 100
 
-for train_index, test_index in kf.split(x_scaled,y):
+for train_index, test_index in kf.split(x_imputed,y):
 
-    # sm = SMOTE()
-    # x_resampled, y_resampled = sm.fit_resample(x_scaled[train_index], y[train_index])
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_imputed[train_index])
+    y_train = y[train_index]
     
-    x_train = torch.from_numpy(x_scaled[train_index])
-    x_test = torch.from_numpy(x_scaled[test_index])
-    y_train = torch.from_numpy(y[train_index])
-    y_test = torch.from_numpy(y[test_index])
+    x_test = scaler.transform(x_imputed[test_index])
+    y_test = y[test_index]
+    
+    x_train,y_train = SMOTE().fit_resample(x_train,y_train)
+    
+    x_train = torch.from_numpy(x_train)
+    x_test = torch.from_numpy(x_test)
+    y_train = torch.from_numpy(y_train)
+    y_test = torch.from_numpy(y_test)
    
     train_loss = []
     val_loss = []
@@ -122,6 +129,6 @@ for train_index, test_index in kf.split(x_scaled,y):
     plt.show()
 
 
-np.save('results/probs/mlp_probs.npy',mlp_probs)
-np.save('results/probs/mlp_test_labels.npy',test_labels)
-torch.save(model,'mlp.pt')
+np.save('results/probs/smote/mlp_probs.npy',mlp_probs)
+np.save('results/probs/smote/mlp_test_labels.npy',test_labels)
+# torch.save(model,'mlp.pt')

eICU/xgboost_shap.py
@@ -18,7 +18,7 @@ y = np.load('data/y.npy')
 
 sm = SMOTE()
 
-x_imputed, y_imputed = sm.fit_resample(x_scaled,y)
+x_resampled, y_resampled = sm.fit_resample(x_scaled,y)
 
 
 feat_names = np.load('data/column_names.npy',allow_pickle=True)
@@ -29,6 +29,6 @@ model = xgboost.train({"gamma":0,"learning_rate": 0.01,"max_depth":2,"min_child_
 # explain the model's predictions using SHAP
 # (same syntax works for LightGBM, CatBoost, scikit-learn and spark models)
 explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(x_imputed)
+shap_values = explainer.shap_values(x_scaled)
 
-shap.summary_plot(shap_values, x_imputed, plot_type="bar")
+shap.summary_plot(shap_values, x_scaled, plot_type="bar")